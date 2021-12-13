98eedb535f91b6081bb9882e05df84772e70b673
Ian Jonsen <ian.jonsen@mq.edu.au>
2021-12-13 13:21:37 -0400

    more tweaking - hopefully reached the final little penguijn example...

--- code/ex33_lipe_final.R
+++ code/ex33_lipe_final.R
@@ -1,44 +1,35 @@
+require(tidyverse)
 peng <- readRDS("data/tracks4ian_20190109.RDS") %>%
   rename(date = dtUTC) %>%
   filter(id %in% c("G123f02",
-                   "G123m06",
-                   "G123m06",
                    "G124m10",
                    "G126m05",
-                   "G128f06",
+                   #"L002m01",
+                   #"L001m01",
+                   "L008m01",
+                   #"L012f01",
                    "L013m01")) %>%
-  mutate(id = as.character(id))
+  mutate(id = as.character(id)) %>%
+  filter((id == "L008m01" & date < ISOdatetime(2017,10,08,04,00,00,tz="UTC")
+          | (id != "L008m01"))) #%>%
+#  filter((id == "L012f01" & date < ISOdatetime(2017,10,13,06,00,00,tz="UTC")
+ #         | (id != "L012f01")))
 
-peng.sub <- peng %>%
+## Due to the penguins' frequent diving, GPS tags were set at a very high sampling rate (approximately 3 s) to minimise artefacts caused by interruptions of the GPS signal. To make this high-volume data more manageable, we first sub-sampled the raw GPS data by retaining every 5th observation from each track, reducing observation frequency to approximately 15 s.
+
+## subsample to similar avg observation freq
+psL <- peng %>%
+  filter(!id %in% c("G123f02","G124m10","G126m05")) %>%
   group_by(id) %>%
-  amt::make_track(., lon, lat, date, id=id) %>%
-  amt::track_resample(rate = minutes(5), tolerance = seconds(10)) %>%
-  select(id, date=t_, lon=x_, lat=y_) %>%
+  slice(seq(1, nrow(.), by = 8)) %>%
   ungroup()
-
-fmp <- fit_mpm(peng.sub, model = "jmpm")
-
-fit <- peng %>%
+psG <- peng %>%
+  filter(id %in% c("G123f02","G124m10","G126m05")) %>%
   group_by(id) %>%
   slice(seq(1, nrow(.), by = 5)) %>%
-  ungroup() %>%
+  ungroup()
+peng.sub <- bind_rows(psL, psG) %>%
   mutate(lc = "G") %>%
-  select(id,date,lc,lon,lat) %>%
-  fit_ssm(
-    .,
-    min.dt = 4,
-    model = "crw",
-    time.step = 5 / 60
-  )
+  select(id,date,lc,lon,lat)
 
-fmp.crw <- fit_mpm(fit, what="p", model = "jmpm")
-
-
-p1 <- plot(fmp, pages = 1, ncol = 1, ask = FALSE, pal = "Cividis") &
-  theme(legend.position = "none")
-
-p2 <- plot(fmp.crw, pages = 1, ncol = 1, ask = FALSE, pal = "Cividis") &
-  theme(legend.position = "none")
-
-
-
+saveRDS(peng.sub, file = "data/ex33_lipe_gps_tracks.RDS", compress="xz")
